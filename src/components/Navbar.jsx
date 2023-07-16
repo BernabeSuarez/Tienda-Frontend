3459f47599e790d944bc221f5bafcf79e18df66f
Bernabe <bernatattoo@gmail.com>
2023-07-16 20:07:01 -0300

    Add cart context, badge navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,19 @@ import {
   Image,
   Spacer,
   useDisclosure,
+  chakra,
 } from "@chakra-ui/react";
 import { BsCart3, BsSearch } from "react-icons/bs";
 import { BiUser } from "react-icons/bi";
 import Cart from "./Cart";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [cart, setCart] = useContext(CartContext);
+
+  const quantity = cart.reduce((acc, curr) => acc + curr.quantity, 0);
 
   return (
     <Flex
@@ -30,7 +36,25 @@ export default function Navbar() {
       <Flex justifyContent="space-between" alignItems="center" w="10%">
         <Icon as={BsSearch} _hover={{ cursor: "pointer" }} />
         <Icon as={BiUser} _hover={{ cursor: "pointer" }} />
-        <Icon as={BsCart3} _hover={{ cursor: "pointer" }} onClick={onOpen} />
+        <Box pos="relative" display="inline-block">
+          <Icon as={BsCart3} _hover={{ cursor: "pointer" }} onClick={onOpen} />
+          <Box
+            pos="absolute"
+            top="-1px"
+            right="-1px"
+            px={1}
+            py={0.5}
+            fontSize="xs"
+            fontWeight="bold"
+            lineHeight="none"
+            color="red.100"
+            transform="translate(50%,-50%)"
+            bg="red.600"
+            rounded="full"
+          >
+            {quantity}
+          </Box>
+        </Box>
       </Flex>
       <Cart isOpen={isOpen} onClose={onClose} />
     </Flex>
