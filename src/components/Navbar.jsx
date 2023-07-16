faf0ba77664e5f1bf3793274eae88d34d29d1bf1
Bernabe <bernatattoo@gmail.com>
2023-07-16 03:29:08 -0300

    Add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Icon,
+  Box,
+  Image,
+  Spacer,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { BsCart3, BsSearch } from "react-icons/bs";
+import { BiUser } from "react-icons/bi";
+import Cart from "./Cart";
 
 export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Flex h="60px" bg="black" color="white" w="100%">
-      Navbar
+    <Flex
+      h="60px"
+      bg="black"
+      color="white"
+      p="0 3%"
+      w="100%"
+      justifyContent="space-evenly"
+      alignItems="center"
+    >
+      <Box w="30%">
+        <Image src="img/logoTienda.png" alt="Logo" w="250px" />
+      </Box>
+      <Spacer />
+      <Flex justifyContent="space-between" alignItems="center" w="10%">
+        <Icon as={BsSearch} _hover={{ cursor: "pointer" }} />
+        <Icon as={BiUser} _hover={{ cursor: "pointer" }} />
+        <Icon as={BsCart3} _hover={{ cursor: "pointer" }} onClick={onOpen} />
+      </Flex>
+      <Cart isOpen={isOpen} onClose={onClose} />
     </Flex>
   );
 }
