db1ac54d628aebb48fcfcb491eeb34e2ce528f0d
Bernabe <bernatattoo@gmail.com>
2023-07-16 20:07:16 -0300

    add cart provider

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,26 @@ import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./components/Rootlayout";
+import { ShoppingCartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route element={<ProtectedRoute />}>
-            <Route element={<RootLayout />}>
-              <Route path="/catalogo" element={<Catalogo />} />
+    <ShoppingCartProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<ProtectedRoute />}>
+              <Route element={<RootLayout />}>
+                <Route path="/catalogo" element={<Catalogo />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </ShoppingCartProvider>
   );
 }
 
